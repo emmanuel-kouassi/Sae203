e90d0d5e00296fecbd2e42a3acd1f717c9a67c4a
emmanuel-kouassi <115140578+emmanuel-kouassi@users.noreply.github.com>
2024-06-03 20:44:28 +0200

    Delete Kouassi_Eddabachi/Kouassi_Eddabachi directory

--- Kouassi_Eddabachi/Kouassi_Eddabachi/TD.js
+++
@@ -1,9 +0,0 @@
-function showTable() {
-    var select = document.getElementById("tpSelect");
-    var value = select.value;
-    var tables = document.getElementsByClassName("table-container");
-    for (var i = 0; i < tables.length; i++) {
-        tables[i].style.display = "none";
-    }
-    document.getElementById(value).style.display = "block";
-}
